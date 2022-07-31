af1a12f63a408870e07a85a6d83fb2e06621a826
Jesus-sauve <68485025+nayoDavid1@users.noreply.github.com>
2022-07-31 11:26:26 +0100

    validate

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     APP_NAME: 'BaseBiblique',
     API_DEVELOPMENT: 'http://localhost:8001/api',
     API_PRODUCTION: 'https://basebiblique.org/api',
-    PRODUCTION: false,
+    PRODUCTION: true,
     DOMAIN_DEVELOPMENT: 'http://localhost:3001',
     DOMAIN_PRODUCTION: 'https://basebiblique.org',
     DISQUS_SHORTNAME: 'basebiblique'
